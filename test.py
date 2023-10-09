f2299f7457941997d425159f5b9fb368254e647f
Antoine A <antoine_avenia@hotmail.fr>
2023-10-09 17:33:21 +0200

    Add counter level + round + timer (10s)

--- test.py
+++ test.py
@@ -1,74 +1,132 @@
 from random import randrange
+import threading
 
-game_over = False
+count_level = 1
+your_number = 0
+count_round = 0
 
-# Level 1
+while True:
 
-print("Level 1")
-level_1 = randrange(1, 10)
+    def input_with_timeout(prompt, timeout):
+        print(prompt)
+        timer = threading.Timer(timeout, lambda: print("\nGame Over") or exit())
+        try:
+            timer.start()
+            return int(input())
+        finally:
+            timer.cancel()
 
-for try_level_1 in range(3):
-    your_number = input('Choisissez un nombre entre 1 et 10 : ')
-    your_number = int(your_number)
-    
-    if your_number == level_1:
-        print("You win")
+    # Choix à l'utilisateur de continuer ou arrêter le jeu
+    def continue_or_quit(count_level):
+        input_user = input("LEVEL X, Il vous reste X solde,Voulez-vous continuer ? (y/n) : ")
+        if input_user == "n":
+            print("Game Over")
+            exit()
+
+    # Level 1
+
+    print("Round", count_round)
+
+    if count_level == 1:
+
+        if count_round > 0:
+            continue_or_quit(count_level)
+
+        print("Level 1")
+        level_1 = randrange(1, 10)
+        print(level_1)
+
+        for try_level_1 in range(3):
+            try:
+                while True :
+                    your_number = input_with_timeout('Choisissez un nombre entre 1 et 10 : ', 10)
+                    if your_number >= 1 and your_number < 10:
+                        break
+
+            except ValueError:
+                print("Entrée invalide, ce n'est pas un nombre.")
+                continue
+            
+            if your_number == level_1:
+                print("You win")
+                count_round += 1
+                count_level += 1
+                break
+            elif your_number > level_1:
+                print("Too high")
+            elif your_number < level_1:
+                print("Too low")
+        else:
+            print("You lose")
+            count_level -= 1
+
+    # Level 2
+
+    if count_level == 2:
+
+        continue_or_quit(count_level)
+
+        print("Level 2")
+        level_2 = randrange(1, 20)
+        print(level_2)
+
+        for try_level_2 in range(5):
+            try:
+                while True :
+                    your_number = input_with_timeout('Choisissez un nombre entre 1 et 20 : ', 10)
+                    if your_number >= 1 and your_number < 20:
+                        break
+            except ValueError:
+                print("Entrée invalide, ce n'est pas un nombre.")
+                continue
+            
+            if your_number == level_2:
+                print("You win")
+                count_round += 1
+                count_level += 1
+                break
+            elif your_number > level_2:
+                print("Too high")
+            elif your_number < level_2:
+                print("Too low")
+        else:
+            print("You lose")
+            count_level -= 1
+
+    # Level 3
+
+    if count_level == 3:
+
+        continue_or_quit(count_level)
+
+        print("Level 3")
+        level_3 = randrange(1, 30)
+        print(level_3)
+
+        for try_level_3 in range(7):
+            try:
+                while True :
+                    your_number = input_with_timeout('Choisissez un nombre entre 1 et 30 : ', 10)
+                    if your_number >= 1 and your_number < 30:
+                        break
+            except ValueError:
+                print("Entrée invalide, ce n'est pas un nombre.")
+                continue
+            
+            if your_number == level_3:
+                print("You win")
+                count_round += 1
+                count_level += 1
+                break
+            elif your_number > level_3:
+                print("Too high")
+            elif your_number < level_3:
+                print("Too low")
+        else:
+            print("You lose")
+            count_level -= 1
+
+    # Game over
+    if count_level == 0:
+        print("Game Over")
         break
-    elif your_number > level_1:
-        print("Too high")
-    elif your_number < level_1:
-        print("Too low")
-else:
-    print("You lose")
-    game_over = True
-
-# Level 2
-
-if game_over == False :
-
-    print("Level 2")
-    
-    level_2 = randrange(1, 20)
-
-    for try_level_2 in range(5):
-        your_number = input('Choisissez un nombre entre 1 et 20 : ')
-        your_number = int(your_number)
-        
-        if your_number == level_2:
-            print("You win")
-            break
-        elif your_number > level_2:
-            print("Too high")
-        elif your_number < level_2:
-            print("Too low")
-    else:
-        print("You lose")
-        game_over = True
-
-
-# Level 3
-
-if game_over == False :
-
-    print("Level 3")
-
-    level_3 = randrange(1, 30)
-
-    for try_level_3 in range(7):
-        your_number = input('Choisissez un nombre entre 1 et 30 : ')
-        your_number = int(your_number)
-        
-        if your_number == level_3:
-            print("You win")
-            break
-        elif your_number > level_3:
-            print("Too high")
-        elif your_number < level_3:
-            print("Too low")
-    else:
-        print("You lose")
-        game_over = True
-
-
-if game_over == True:
-    print("Game over")
