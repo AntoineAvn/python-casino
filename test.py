79f9c4b23e5d1e57e568b5d737340a7b2d81bbcf
Antoine A <antoine_avenia@hotmail.fr>
2023-10-10 09:44:35 +0200

    add solde + mise

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 from random import randrange
 import threading
+from statsClass import Stats, readData
 
 count_level = 1
 your_number = 0
 count_round = 0
+solde = 0
+mise = 5
 
 while True:
 
@@ -17,24 +20,39 @@ while True:
             timer.cancel()
 
     # Choix à l'utilisateur de continuer ou arrêter le jeu
-    def continue_or_quit(count_level):
-        input_user = input("LEVEL X, Il vous reste X solde,Voulez-vous continuer ? (y/n) : ")
+    def continue_or_quit(count_level, solde):
+        input_user = input("Vous êtes au level " + str(count_level) + ", Il vous reste " + str(solde) + " euro, Voulez-vous continuer ? (y/n) : ")
         if input_user == "n":
             print("Game Over")
             exit()
 
-    # Level 1
+    # Choix à l'utilisateur de rentrer un solde au début du jeu
+    if count_round == 0:
+        try:
+            while True :
+                print("Round", count_round)
 
-    print("Round", count_round)
+                solde = int(input("Choisissez un solde entre 1 et 100 : "))
+                if solde >= 1 and solde < 100:
+                    break
+        except ValueError:
+            print("Entrée invalide, ce n'est pas un nombre.")
+            continue
+
+
+    # Level 1 (gagne la mise)
 
     if count_level == 1:
 
         if count_round > 0:
-            continue_or_quit(count_level)
+            continue_or_quit(count_level, solde)
 
         print("Level 1")
         level_1 = randrange(1, 10)
         print(level_1)
+
+        solde -= mise
+        print('solde : ', solde)
 
         for try_level_1 in range(3):
             try:
@@ -51,6 +69,7 @@ while True:
                 print("You win")
                 count_round += 1
                 count_level += 1
+                solde += 5
                 break
             elif your_number > level_1:
                 print("Too high")
@@ -60,15 +79,18 @@ while True:
             print("You lose")
             count_level -= 1
 
-    # Level 2
+    # Level 2 (gagne la mise * 2)
 
     if count_level == 2:
 
-        continue_or_quit(count_level)
+        continue_or_quit(count_level, solde)
 
         print("Level 2")
         level_2 = randrange(1, 20)
         print(level_2)
+
+        solde -= mise
+        print('solde : ', solde)
 
         for try_level_2 in range(5):
             try:
@@ -84,6 +106,7 @@ while True:
                 print("You win")
                 count_round += 1
                 count_level += 1
+                solde += mise * 2
                 break
             elif your_number > level_2:
                 print("Too high")
@@ -93,15 +116,18 @@ while True:
             print("You lose")
             count_level -= 1
 
-    # Level 3
+    # Level 3 (gagne la mise * 4)
 
     if count_level == 3:
 
-        continue_or_quit(count_level)
+        continue_or_quit(count_level, solde)
 
         print("Level 3")
         level_3 = randrange(1, 30)
         print(level_3)
+
+        solde -= mise
+        print('solde : ', solde)
 
         for try_level_3 in range(7):
             try:
@@ -117,6 +143,8 @@ while True:
                 print("You win")
                 count_round += 1
                 count_level += 1
+                solde += mise * 4
+                print('Bravo vous avez gagné ! Votre solde final est de : ' + str(solde))
                 break
             elif your_number > level_3:
                 print("Too high")
